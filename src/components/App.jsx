db69520671f31745e8e8d21289ac4ca9c388f693
polonkoev <polonkoev.temerlan@gmail.com>
2022-11-24 12:07:00 +0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+import { Section } from './Section/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <>
+        <Section title={'Please leave feedback'} />
+      </>
+    );
+  }
+}
+export default App;
