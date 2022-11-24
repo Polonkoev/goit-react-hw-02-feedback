19b96d9e83ee483a4b4488e7951b45123b10eb9f
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-11-24 21:58:34 +0600

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
 import { Component } from 'react';
 
 import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 10,
+    neutral: 10,
+    bad: 10,
   };
 
+  countTotalFeedback = () => {
+    return this.setState( parseInt(this.good + this.neutral + this.bad));
+  };
+
+  countPositiveFeedbackPercentage = () => {
+   return this.setState(parseInt(this.good / this.countTotalFeedback * 100))
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <>
-        <Section title={'Please leave feedback'} />
+        <Section title={'Please leave feedback'} children>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+          <Notification message={'There is no feedback'} />
+        </Section>
       </>
     );
   }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,17 @@
-export const Section = ({title}) => {
+export const Section = ({title, children}) => {
   return (
-  <h2>{title}</h2>
+  <section style={{
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    margin: 20,
+    padding: "20px 16px",
+    borderRadius: 4,
+    backgroundColor: "rgb(255, 228, 196)",
+    color: "#130202",
+  }}>
+    <h2>{title}</h2>
+    {children}
+  </section>
   );
 };
